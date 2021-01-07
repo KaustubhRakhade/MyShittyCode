515e5e62eefabad643a854f2f54f801ad13eb271
KaustubhRakhade <77070660+KaustubhRakhade@users.noreply.github.com>
2021-01-07 08:36:28 +0530

    Add files via upload

---
+++ HelloWorld1.py
@@ -0,0 +1,17 @@
+class System:
+    class out:
+        class java:
+            class utils:
+                class windows:
+                    class mac:
+                        class linux:
+                            class iphone:
+                                class android:
+                                    class china:
+                                        def println(text):
+                                            print(text)
+ 
+#------------------------------------------
+#Do it the Java Way
+
+System.out.java.utils.windows.mac.linux.iphone.android.china.println("Hello World!");
---
+++ HelloWorld2.py
@@ -0,0 +1,12 @@
+import os as ATraditionOfImportingUselessStuff
+
+def PrintIntoTheConsoleWindowAllTheTextInThisBox(TheStringOfTextThatYouWantToPrintInTheConsoleWindow):
+    Samuel = "Noob"
+    while str(int(0)) == str(1-1):
+        import random as APythonModuleForGeneratingPseudoRandomNumbersDecimalsIntegersAndMuchMore
+        if APythonModuleForGeneratingPseudoRandomNumbersDecimalsIntegersAndMuchMore.randint(8,10) == APythonModuleForGeneratingPseudoRandomNumbersDecimalsIntegersAndMuchMore.randint(10,12):
+            break
+    if Samuel == "Noob":
+           print(TheStringOfTextThatYouWantToPrintInTheConsoleWindow)
+
+PrintIntoTheConsoleWindowAllTheTextInThisBox("Hello World!")
