7a474941f81ccfb63e01e5fce126214fbc8003f3
KaustubhRakhade <77070660+KaustubhRakhade@users.noreply.github.com>
2021-01-09 17:19:14 +0530

    Update HelloWorld2.py

--- HelloWorld2.py
+++ HelloWorld2.py
@@ -10,15 +10,4 @@ def PrintIntoTheConsoleWindowAllTheTextInThisBox(TheStringOfTextThatYouWantToPri
     if Samuel == "Noob":
            print(TheStringOfTextThatYouWantToPrintInTheConsoleWindow)
 
-PrintIntoTheConsoleWindowAllTheTextInThisBox("Hello World!")mport os as ATraditionOfImportingUselessStuff
-
-def PrintIntoTheConsoleWindowAllTheTextInThisBox(TheStringOfTextThatYouWantToPrintInTheConsoleWindow):
-    Samuel = "Noob"
-    while str(int(0)) == str(1-1):
-        import random as APythonModuleForGeneratingPseudoRandomNumbersDecimalsIntegersAndMuchMore
-        if APythonModuleForGeneratingPseudoRandomNumbersDecimalsIntegersAndMuchMore.randint(0,10) == APythonModuleForGeneratingPseudoRandomNumbersDecimalsIntegersAndMuchMore.randint(10,20):
-            break
-    if Samuel == "Noob":
-           print(TheStringOfTextThatYouWantToPrintInTheConsoleWindow)
-
 PrintIntoTheConsoleWindowAllTheTextInThisBox("Hello World!")
